222a78f8a0775d824430b853add82b83a7cc35f4
Mahmoudelhawar <132308204+Mahmoudelhawar@users.noreply.github.com>
2023-05-02 01:05:04 +0300

    Add files via upload

---
+++ sports/mh.js
@@ -0,0 +1,10 @@
+function showLineup() {
+    const goalkeeper = "جيانلويجي دوناروما";
+    const defense = "دافيدي كالابريا، فرانك كيسي، سيمون كير، ثيو هيرنانديز";
+    const midfield = "إيسماعيل بن ناصر، ساندرو تونالي، فرانك كيسي";
+    const attack = "زلاتان إبراهيموفيتش، أوليفييه جيرو، براهيم دياز";
+    document.getElementById("goalkeeper").innerHTML = goalkeeper;
+    document.getElementById("defense").innerHTML = defense;
+    document.getElementById("midfield").innerHTML = midfield;
+    document.getElementById("attack").innerHTML = attack;
+  }
